8f23c85d1001e6f3b5afd7e9c608e8244a69c165
Rami Karan <99781533+ksrami99@users.noreply.github.com>
2025-07-26 10:50:19 +0530

    feat: add multer middleware for file uploads

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,18 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/images");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "_" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+export const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1 * 1000 * 1000,
+  },
+});
